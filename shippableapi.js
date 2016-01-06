cd608bc2b38aae542051eecfe7487d76acb4fae9
Chris Kinsman <chris@kinsman.net>
2016-01-06 08:27:53 -0800

    Initial workflow functions.  Untested and waiting on shippable to respond to issue #2177 on workflow issues

--- shippableapi.js
+++ shippableapi.js
@@ -3,29 +3,33 @@
 var util = require('util');
 var log = util.debuglog('shippable');
 var url = require('url');
+
+var async = require('async');
 var request = require('request');
 var _ = require('lodash');
+
 
 function ShippableApi(apiToken, options) {
     options = options || {};
     this.baseUrl = options.baseUrl || 'https://api.shippable.com/';
     this.apiToken = apiToken;
-}
-
-ShippableApi.prototype._get = function get(path, queryParams, callback) {
-    var params = {
-        url: url.resolve(this.baseUrl, path),
+    this.projectToName = {};
+    this.baseParams = {
         headers: {
             'Authorization': 'apiToken: ' + this.apiToken
         },
         json: true
     };
+}
 
-    if(queryParams) {
-        params.qs =queryParams;
-    }
+ShippableApi.prototype._get = function _get(path, queryParams, callback) {
+    queryParams = queryParams || {};
+    var params = {
+        url: url.resolve(this.baseUrl, path),
+        qs: queryParams
+    };
 
-    request.get(params, function(err, resp, body) {
+    request.get(_.extend(params, this.baseParams), function(err, resp, body) {
         if(err || resp.statusCode!==200) {
             log('get err: %s, body: %s, statusCode: %d', err, body, resp.statusCode);
             if(!err) {
@@ -36,6 +40,25 @@ ShippableApi.prototype._get = function get(path, queryParams, callback) {
     });
 };
 
+ShippableApi.prototype._post = function _post(path, body, callback) {
+    var params = {
+        url: url.resolve(this.baseUrl, path),
+        body: body
+    };
+
+    console.dir(params);
+    request.post(_.extend(params, this.baseParams), function(err, resp, body) {
+        if(err || resp.statusCode!==200) {
+            log('get err: %s, body: %s, statusCode: %d', err, body, resp.statusCode);
+            if(!err) {
+                err = 'StatusCode: ' + resp.statusCode;
+            }
+        }
+        callback(err, body);
+    });
+};
+
+
 ShippableApi.prototype.getProjects = function getProjects(callback) {
     this._get('/projects', null, callback);
 };
@@ -44,8 +67,48 @@ ShippableApi.prototype.getProject = function getProject(projectId, callback) {
     this._get('/projects/' + projectId, null, callback);
 };
 
+ShippableApi.prototype.getProjectByName = function getProjectByName(projectName, callback) {
+    var self = this;
+    async.waterfall([
+        function lookupProjectName(done) {
+            var projectId = self.projectToName[projectName];
+            if(projectId) {
+                return setImmediate(function(){ done(null, projectId);});
+            }
+            else {
+                self.getProjects(function(err, projects) {
+                    if(!err) {
+                        _.each(projects, function(project) {
+                            self.projectToName[project.name] = project.id;
+                        });
+                    }
+
+                    var projectId = self.projectToName[projectName];
+                    if(projectId) {
+                        return setImmediate(function(){ done(null, projectId);});
+                    }
+                    else {
+                        return setImmediate(function(){ done('Project name not found');});
+                    }
+                });
+            }
+        },
+        function getProject(projectId, done) {
+            self.getProject(projectId, done);
+        }
+    ], callback);
+};
+
 ShippableApi.prototype.searchBuilds = function searchBuilds(projectId, queryParams, callback) {
     this._get('/projects/' + projectId + '/searchBuilds', queryParams, callback);
+};
+
+ShippableApi.prototype.enableBuild = function enableBuild(projectId, callback) {
+    this._post('/workflow/enableRepoBuild', {projectId: projectId}, callback);
+};
+
+ShippableApi.prototype.disableBuild = function disableBuild(projectId, callback) {
+    this._post('/workflow/disableBuild', {projectId: projectId}, callback);
 };
 
 module.exports = ShippableApi;
--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var async = require('async');
+
 var ShippableApi = require('../shippableapi');
 var shippableApi = new ShippableApi(process.env.SHIPPABLE_TOKEN);
 
@@ -21,14 +23,56 @@ Tests.getProject = function getProject(test) {
     });
 };
 
+Tests.getProjectByName = function getProjectByName(test) {
+    shippableApi.getProjectByName('eventotron', function(err, project) {
+        test.ifError(err);
+        test.ok(project!==null, 'No project returned: ' + project.name);
+        test.equal('eventotron', project.name, 'Wrong project returned');
+        test.done();
+    })
+};
+
+
 Tests.searchBuilds = function searchBuilds(test) {
     shippableApi.searchBuilds('5490c415d46935d5fbc061b5', {limit: 1}, function(err, builds) {
         test.ifError(err);
-        console.dir(builds);
         test.ok(builds!==null,'Builds null');
         test.ok(builds.length===1, 'Limit failed');
         test.done();
     });
 };
+
+Tests.enableBuild = function enableBuild(test) {
+    async.waterfall([
+        function getProjectId(done) {
+            shippableApi.getProjectByName('s3-stream-upload', function(err, project) {
+                console.info(project.id);
+                done(err, project.id);
+            })
+        },
+        function enableBuild(projectId, done) {
+            shippableApi.enableBuild(projectId, done);
+        }
+    ], function(err) {
+        test.ifError(err);
+    });
+};
+
+Tests.disableBuild = function disableBuild(test) {
+    async.waterfall([
+        function getProjectId(done) {
+            shippableApi.getProjectByName('s3-stream-upload', function(err, project) {
+                console.info(project.id);
+                done(err, project.id);
+            })
+        },
+        function disableBuild(projectId, done) {
+            shippableApi.disableBuild(projectId, done);
+        }
+    ], function(err) {
+        test.ifError(err);
+    });
+};
+
 
 module.exports = Tests;
